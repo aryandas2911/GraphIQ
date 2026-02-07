e27e352c47841bc34f441755c760dce6278c4727
Aryan Das <aryandas2911@gmail.com>
2026-02-08 01:52:26 +0530

    Register page completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,29 @@
-import Navbar from "./components/Navbar/Navbar.jsx"
-import LandingPage from "./pages/LandingPage"
+import { Routes, Route, Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import LandingPage from "./pages/LandingPage";
+import Signup from "./pages/Signup.jsx";
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <LandingPage/>
-    </div>
-  )
-}
+    <>
+      <Routes>
+        {/* Public layout (with navbar) */}
+        <Route
+          element={
+            <>
+              <Navbar />
+              <Outlet />
+            </>
+          }
+        >
+          <Route path="/" element={<LandingPage />} />
+        </Route>
 
-export default App
+        {/* Auth layout (no navbar) */}
+        <Route path="/register" element={<Signup />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
