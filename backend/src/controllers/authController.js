b4b55affc418ec8f7c844d264fa5359e8a3c2b3c
Aryan Das <aryandas2911@gmail.com>
2026-05-06 13:17:55 +0530

    Login functionality added

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,16 +1,20 @@
 import bcrypt from "bcryptjs";
 import { supabase } from "../config/db.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const signup = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
-    const { data:existingUser, error:existingError } = await supabase
+    const { data: existingUser, error: existingError } = await supabase
       .from("users")
       .select("*")
       .eq("email", email);
 
-    if (existingUser && existingUser.length>0) {
+    if (existingUser && existingUser.length > 0) {
       return res.status(403).json({ message: "User already exists" });
     }
 
@@ -18,7 +22,7 @@ export const signup = async (req, res) => {
       return res.status(400).json({ message: "Invalid Inputs" });
     }
 
-    if (!(password.length >= 6)) {
+    if (password.length < 6) {
       return res
         .status(400)
         .json({ message: "Password should be atleast 6 characters" });
@@ -27,7 +31,7 @@ export const signup = async (req, res) => {
     const salt = await bcrypt.genSalt();
     const password_hash = await bcrypt.hash(password, salt);
 
-    const { data:newUser, error:newError } = await supabase
+    const { data: newUser, error: newError } = await supabase
       .from("users")
       .insert([{ name: name, email: email, password_hash: password_hash }])
       .select("id,name,email,created_at");
@@ -36,7 +40,56 @@ export const signup = async (req, res) => {
       return res.status(500).json({ message: "Database error" });
     }
 
-    return res.status(200).json({ message: "User registered", newUser });
+    const user=newUser[0];
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" },
+    );
+
+    return res.status(200).json({ message: "User registered", token, newUser });
+  } catch (error) {
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Invalid Inputs" });
+    }
+
+    const { data: userData, error: loginError } = await supabase
+      .from("users")
+      .select("*")
+      .eq("email", email);
+
+    if (loginError || !userData || userData.length === 0) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const user = userData[0];
+
+    const isMatch = await bcrypt.compare(password, user.password_hash);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" },
+    );
+
+    return res.status(200).json({
+      message: "User logged in",
+      token,
+      user: { id: user.id, name: user.name, email: user.email },
+    });
   } catch (error) {
     return res.status(500).json({ message: "Server error" });
   }
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { signup } from "../controllers/authController.js";
+import { login, signup } from "../controllers/authController.js";
 
 export const authRouter = express.Router();
 
-authRouter.post('/signup',signup)
+authRouter.post("/signup", signup);
+
+authRouter.post("/login", login);
